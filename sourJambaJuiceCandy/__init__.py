753f72dc57bacec4d88b7764b8ab0f82a396d522
Ibnul Jahan <ijahan1@stuy.edu>
2018-05-29 10:57:27 -0400

    Form adds to database, and notes queue displays some database indormation

--- sourJambaJuiceCandy/__init__.py
+++ sourJambaJuiceCandy/__init__.py
@@ -64,7 +64,7 @@ Making an account is not really a thing (by design). All accounts should be made
 @app.route('/')
 def root():
     if 'user' in session:
-        return render_template('index.html', username = session['user'], in_session = in_session(), is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), dir = DIR)
+        return render_template('index.html', user_id = session["user_id"], username = session['user'], first_name = session["first_name"], last_name = session["last_name"], email = session["email"], type = session["type"], in_session = in_session(), is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), dir = DIR)
     return render_template('index.html',  in_session = in_session(), is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), dir = DIR)
     
 
@@ -84,12 +84,12 @@ def activate():
     if request.method == 'POST':
         if data.activateable(request.form['email'], request.form['code']):
             error = "Your account does exist"
-            session['user_id'] = data.check_account(request.form['email'], request.form['code'])[0]
-            session['user'] = data.check_account(request.form['email'], request.form['code'])[1]
-            session['first_name'] = data.check_account(request.form['email'], request.form['code'])[2]
-            session['last_name'] = data.check_account(request.form['email'], request.form['code'])[3]
-            session['email'] = data.check_account(request.form['email'], request.form['code'])[4]
-            session['type'] = data.check_account(request.form['email'], request.form['code'])[5]
+            session['user_id'] = data.check_account(request.form['email'], request.form['code'])[1]
+            session['user'] = data.check_account(request.form['email'], request.form['code'])[2]
+            session['first_name'] = data.check_account(request.form['email'], request.form['code'])[3]
+            session['last_name'] = data.check_account(request.form['email'], request.form['code'])[4]
+            session['email'] = data.check_account(request.form['email'], request.form['code'])[5]
+            session['type'] = data.check_account(request.form['email'], request.form['code'])[6]
             
             return render_template("create_password.html")
         else:
@@ -104,7 +104,8 @@ def create_password():
     if (request.form['password'] == request.form['password_verif']):
         print session['user'] , request.form['password'] 
         newpass = data.activate_account( session['user'] , request.form['password'] )
-        return render_template("index.html", is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), username = session['user'], in_session = in_session(), dir = "Not a dir, but the new password is  ")
+        ###return render_template("index.html", is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), username = session['user'], in_session = in_session(), dir = "Not a dir, but the new password is  ")
+        return redirect(url_for('root'))
     return render_template("failed_login.html", error = "Passwords did not match")
 
 
@@ -164,7 +165,8 @@ def logout():
 @app.route('/notes_queue')
 def notes_queue():
     if in_session():
-        return render_template("notes_queue.html")
+        absences = data.retrieve_absences( session["user_id"] )
+        return render_template("notes_queue.html", all_absences = absences)
     return render_template("index.html", is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), error = "You are not logged in")
 
 
@@ -178,10 +180,38 @@ def new_form():
 
 @app.route('/submit_form' , methods=['POST','GET'])
 def submit_form():
+    print "on submit form"
     if in_session():
+        
+        #dict = {}
+        #dict[request.form["course_one"]] = request.form["teacher_one"]
+        #dict[request.form["course_two"]] = request.form["teacher_two"]
+        #dict[request.form["course_three"]] = request.form["teacher_three"]
+        #dict[request.form["course_four"]] = request.form["teacher_four"]
+        #dict[request.form["course_five"]] = request.form["teacher_five"]
+        #dict[request.form["course_six"]] = request.form["teacher_six"]
+        #dict[request.form["course_seven"]] = request.form["teacher_seven"]
+        #dict[request.form["course_eight"]] = request.form["teacher_eight"]
+        #dict[request.form["course_nine"]] = request.form["teacher_nine"]
+        #dict[request.form["course_ten"]] = request.form["teacher_ten"]
 
+        class_list = []
+        class_list.append( [ request.form["course_one"], request.form["teacher_one"] ] )
+        class_list.append( [ request.form["course_two"], request.form["teacher_two"] ] )
+        class_list.append( [ request.form["course_three"], request.form["teacher_three"] ] )
+        class_list.append( [ request.form["course_four"], request.form["teacher_four"] ] )
+        class_list.append( [ request.form["course_five"], request.form["teacher_five"] ] )
+        class_list.append( [ request.form["course_six"], request.form["teacher_six"] ] )
+        class_list.append( [ request.form["course_seven"], request.form["teacher_seven"] ] )
+        class_list.append( [ request.form["course_eight"], request.form["teacher_eight"] ] )
+        class_list.append( [ request.form["course_nine"], request.form["teacher_nine"] ] )
+        class_list.append( [ request.form["course_ten"], request.form["teacher_ten"] ] )
+
+        print "are you working here?"
+        
         #need to add way to connect student and parent accounts
-        data.new_note( session["user_id"], request.form["osis"], request.form["osis"], request.form["excuse"], 1, 0, request.form["date"], 0);
+        data.new_note( request.form["osis"], session["user_id"], 99, request.form["excuse"], 1, 0, request.form["date"], 0, class_list);
+        
         return redirect(url_for("notes_queue"))
     return render_template("index.html", is_student = is_student(), is_parent = is_parent(), is_teacher = is_teacher(), error = "You are not logged in")
 
--- sourJambaJuiceCandy/util/database.py
+++ sourJambaJuiceCandy/util/database.py
@@ -72,14 +72,62 @@ def check_account( email, password ):
     if (len(results) == 0):
         print "INCORRECT LOGIN"
         return False, False, False, False, False, False, False
+    #user_id, username, first_name, last_name, email, type
     return True, results[0][0], results[0][1], results[0][3], results[0][4], results[0][5], results[0][6]
     
 
 #add to big notes db
 #create individual note db
-def new_note( id, osis, student_id, parent_id, explanation, signed_by_student, signed_by_parent, date, completed):
+def new_note( osis, student_id, parent_id, explanation, signed_by_student, signed_by_parent, date, completed, class_list):
+
+    f="absence_sys.db"
+    db = sqlite3.connect(f) 
+    c = db.cursor() 
+
+    command = "SELECT COUNT(*) FROM absent_notes"
+    c.execute(command)
+    count = c.fetchone()[0]
+    
+    #this should add to the aggregate note table
+    command = "INSERT INTO absent_notes VALUES(" + str(count) + "," + str(osis) + "," + str(student_id) + "," + str(parent_id) + ",'" + explanation + "'," + str(signed_by_student) + "," + str(signed_by_parent) + ",'" + date + "'," + str(completed) + ")"
+    c.execute(command)
+
+    print "added to big"
+    
+    #this should create an individual note table
+    command = "CREATE TABLE note_" + str(count) + "( course_code TEXT, teacher TEXT, period INTEGER, signed INTEGER )"
+    c.execute(command)
+
+    print "made small"
+    
+    #this should add to the individual note tables
+    for i in xrange(10):
+        print "insertion " + str(i)
+        print  "INSERT INTO note_" + str(count) + " VALUES('" + class_list[i][0] + "','" + class_list[i][1] + "'," + str(i + 1) + ",'' )"
+        command = "INSERT INTO note_" + str(count) + " VALUES('" + class_list[i][0] + "','" + class_list[i][1] + "'," + str(i + 1) + ",0 )"
+        c.execute(command)
+        
+        db.commit()
+        
+
+    print "added to small"
 
 
-    command = "CREATE TABLE note_" + str(id) + "( course_code INTEGER, teacher TEXT, period INTEGER, signed INTEGER )"
-    pass
+
+
+def retrieve_absences( user_id ):
+    f="absence_sys.db"
+    db = sqlite3.connect(f) 
+    c = db.cursor()
+    
+    command = "SELECT * FROM absent_notes WHERE student_id = " + str(user_id) + ""
+    c.execute(command)
+
+    results = c.fetchall()
+    ##results[x] is the xth entry
+    ##results[x][y] is the yth column of the xth entry
+
+    return results
+
+
     
